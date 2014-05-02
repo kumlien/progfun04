471e1edf41cfaf767bf2a983e53d25f638b9fc4b
Svante Kumlien <svante.kumlien@gmail.com>
2014-05-02 23:13:07 +0200

    add lib_managed

--- week1/src/main/scala/recfun/Main.scala
+++ week1/src/main/scala/recfun/Main.scala
@@ -14,15 +14,15 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 0
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = false
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = 0
 }
